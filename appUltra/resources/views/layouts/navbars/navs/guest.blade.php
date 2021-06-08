c46900719cf9072b10a4b613e27afa0d56bfda2e
Renato <emisor.tarea@gmail.com>
2021-06-08 09:50:23 -0600

    Listado de comisiones

--- appUltra/resources/views/layouts/navbars/navs/guest.blade.php
+++ appUltra/resources/views/layouts/navbars/navs/guest.blade.php
@@ -3,12 +3,6 @@
   <div class="container">
     <div class="navbar-wrapper">
       <a class="navbar-brand" href="{{ route('welcome') }}">{{ $title }}</a>
-      <audio autoplay="" loop controls="" preload="auto">
-          <source src="/ultra/audio/tres.mp3" type="audio/mp3">
-          <source src="/ultra/audio/uno.mp3" type="audio/mp3">
-          <source src="/ultra/audio/dos.mp3" type="audio/mp3">
-          Tu navegador no soporta audio HTML5.
-      </audio>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
--- appUltra/resources/views/layouts/page_templates/guest.blade.php
+++ appUltra/resources/views/layouts/page_templates/guest.blade.php
@@ -1,7 +1,13 @@
 @include('layouts.navbars.navs.guest')
 <div class="wrapper wrapper-full-page">
-  <div class="page-header login-page header-filter" filter-color="black" style="background-image: url('{{ asset('material') }}/img/login.webp'); background-size: cover; background-position: top center;align-items: center;" data-color="purple">
-  <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+  <div class="page-header login-page header-filter" filter-color="black" style="background-image: url('{{ asset('material') }}/img/login.webp'); background-size: cover; background-position: top center;align-items: center;" data-color="purple"> 
+    <audio autoplay="" loop controls="" preload="auto">
+        <source src="/ultra/audio/tres.mp3" type="audio/mp3">
+        <source src="/ultra/audio/uno.mp3" type="audio/mp3">
+        <source src="/ultra/audio/dos.mp3" type="audio/mp3">
+        Tu navegador no soporta audio HTML5.
+    </audio>
+    <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
     @yield('content')
     @include('layouts.footers.guest')
   </div>
