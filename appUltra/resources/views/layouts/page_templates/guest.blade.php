2416cec24d710754dc3ce92a4f73b1671280b90f
Renato <emisor.tarea@gmail.com>
2021-06-09 11:22:42 -0600

    Facebook page

--- appUltra/resources/views/layouts/page_templates/guest.blade.php
+++ appUltra/resources/views/layouts/page_templates/guest.blade.php
@@ -4,6 +4,33 @@
     <audio src="{{ asset('audio') }}/cuatro.mp3" loop autoplay preload="auto"></audio>
     <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
     @yield('content')
+    <!-- Messenger plugin de chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your plugin de chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
     @include('layouts.footers.guest')
   </div>
 </div>
+@push('js')
+  <script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "1941669916118053");
+    chatbox.setAttribute("attribution", "biz_inbox");
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v11.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  </script>
+@endpush
