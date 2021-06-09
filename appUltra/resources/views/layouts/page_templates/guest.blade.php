5311b065d4be5dd8b148baf4f81971b8b4856680
Renato <emisor.tarea@gmail.com>
2021-06-09 11:28:01 -0600

    Facebook page

--- appUltra/resources/views/layouts/page_templates/guest.blade.php
+++ appUltra/resources/views/layouts/page_templates/guest.blade.php
@@ -13,24 +13,3 @@
     @include('layouts.footers.guest')
   </div>
 </div>
-@push('js')
-  <script>
-    var chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("page_id", "1941669916118053");
-    chatbox.setAttribute("attribution", "biz_inbox");
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'v11.0'
-      });
-    };
-
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  </script>
-@endpush
